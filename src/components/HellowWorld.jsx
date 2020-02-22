1ee2079a21011877fefdff11cbd66a9e7f7b6d0e
Oscar Daniel <t0d0c4m614@hotmail.com>
2020-02-21 20:14:51 -0500

    ESLint

---
+++ src/components/HellowWorld.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../assets/styles/App.scss';
+
+const HellowWorld = () => {
+    return(
+        <div>
+        <h1>HOLA bb</h1>
+        <h2>ya k contigo no sirv la labia :p</h2>
+    </div>
+    )
+    
+}
+export default HellowWorld;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HellowWorld from './components/HellowWorld';
+
+ReactDOM.render(<HellowWorld />, document.getElementById('app'));
